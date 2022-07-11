b41465e8347272abc0d40342a04a2c1da3afa789
konglongbaba02 <95004945+konglongbaba02@users.noreply.github.com>
2022-07-11 21:42:17 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,14 +4,14 @@
 AUUID=24b4b1e1-7a89-45f6-858c-242cf53bsaul
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
 CONFIGCADDY=https://raw.githubusercontent.com/jth445600/HerokuXray/main/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/jth445600/HerokuXray/master/etc/xray.json
+CONFIGXRAY=https://raw.githubusercontent.com/jth445600/HerokuXray/master/etc/tielu.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
 StoreFiles=https://raw.githubusercontent.com/jth445600/HerokuXray/main/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
 wget -qO- $CONFIGCADDY | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(caddy hash-password --plaintext $AUUID)/g" >/etc/caddy/Caddyfile
-wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/xray.json
+wget -qO- $CONFIGXRAY | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" >/tielu.json
 
 # storefiles
 mkdir -p /usr/share/caddy/$AUUID && wget -O /usr/share/caddy/$AUUID/StoreFiles $StoreFiles
@@ -24,6 +24,6 @@ done
 # start
 tor &
 
-/xray -config /tielu.json &
+/tielu -config /tielu.json &
 
 caddy run --config /etc/caddy/Caddyfile --adapter caddyfile
